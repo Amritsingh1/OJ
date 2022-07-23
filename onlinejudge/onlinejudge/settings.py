830e1c8b37b30aa202e2f4108ec4f8dafcdfa606
Amritsingh1 <76990792+Amritsingh1@users.noreply.github.com>
2022-07-24 02:25:12 +0530

    Update settings.py

--- onlinejudge/onlinejudge/settings.py
+++ onlinejudge/onlinejudge/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5ut1ji@$j!f$wdmy-leu4)@@--hybc&0ay%myemtttw^8#9=!r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
